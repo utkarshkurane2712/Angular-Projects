c6f9bd9c040efd5b0da703522fc138bd4f911c71
Utkarsh Kurane <160477276+utkarshkurane2712@users.noreply.github.com>
2024-02-22 16:04:01 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet,RegisterComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'AngularTask';
+
+  
+}
